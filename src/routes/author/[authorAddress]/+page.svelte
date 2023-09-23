64120045836056beebe7c8c4a74b3c9ed28f7d63
Berzan <berzan@tutanota.com>
2023-09-23 15:54:35 +0000

    create: author page

---
+++ src/routes/author/[authorAddress]/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { authorStore } from '$lib/stores/authorStore';
+	import { wordStore } from '$lib/stores/wordStore.js';
+
+	export let data;
+
+	$: authorInfo = $authorStore[data.authorAddress];
+</script>
+
+<main class="flex flex-col px-4 py-2 gap-6 bg-green-200 border border-green-300 rounded-lg w-full">
+	<a href="/author/{data.authorAddress}">
+		<h1 class="text-4xl font-bold underline">
+			{#if authorInfo}
+				<!-- svelte-ignore a11y-img-redundant-alt -->
+				<img
+					src={authorInfo.profilePhoto}
+					alt="author profile photo"
+					class="h-32 w-32 rounded-full"
+				/>
+			{:else}
+				<p>author does not exist</p>
+			{/if}
+		</h1>
+	</a>
+</main>
