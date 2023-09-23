7249cd1f418cfd62c98e2c935991b5c39eb76489
Berzan <berzan@tutanota.com>
2023-09-23 11:39:35 +0000

    create: Header component

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,4 @@
+<header class="flex justify-between items-center h-16 bg-green-300/70 px-4">
+	<a href="/" class="text-2xl font-bold">Aptos Dictionary</a>
+    <button class="font-bold bg-green-400 hover:bg-green-300 px-4 py-1 rounded-full duration-150">Connect</button>
+</header>
