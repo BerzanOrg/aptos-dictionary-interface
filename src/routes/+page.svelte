3ade58e44bcf1f4e30e180064206f25d04a098ab
Berzan <berzan@tutanota.com>
2023-09-23 15:54:03 +0000

    add: main element

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main class="flex flex-col px-3 py-1 bg-green-200 border border-green-300 rounded-lg w-full">
+	<h1>Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</main>
