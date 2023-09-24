1ba281f963f0fc350caf4257a60f65c44e8f61a3
Berzan <berzan@tutanota.com>
2023-09-24 10:54:45 +0000

    create: main error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<main class="flex flex-col px-3 py-1 bg-green-200 border border-green-300 rounded-lg w-full">
+	<h1>{$page.error?.message}</h1>
+</main>
