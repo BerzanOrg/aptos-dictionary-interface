87337c73840fbf8a084e3933a5388a6800ae191f
Berzan <berzan@tutanota.com>
2023-09-24 10:05:36 +0000

    remove: unused types

--- src/lib/types/window-aptos.ts
+++ src/lib/types/window-aptos.ts
@@ -1,33 +1,28 @@
 declare interface Window {
-    aptos?: {
-        connect: () => Promise<void>
-        disconnect: () => Promise<void>
-        isConnected: () => Promise<boolean>
-        getNetwork: () => Promise<Network>
-        getAccount: () => Promise<Account>
-        onAccountChange: (callback: (a: Account) => Promise<void>) => void
-        onNetworkChange: (callback: (n: Network) => Promise<void>) => void
-        onDisconnect: (callback: () => Promise<void>) => void
-        signAndSubmitTransaction: (tx: Transaction) => Promise<void>
-
-    }
+	aptos?: {
+		connect: () => Promise<Account>;
+		disconnect: () => Promise<void>;
+		isConnected: () => Promise<boolean>;
+		getNetwork: () => Promise<Network>;
+		getAccount: () => Promise<Account>;
+		onAccountChange: (callback: (a: Account) => void) => void;
+		onDisconnect: (callback: () => void) => void;
+		signAndSubmitTransaction: (tx: Transaction) => Promise<void>;
+	};
 }
 
 interface Network {
-    chainId: "1" | "2" | "81" | string
-    name: "Mainnet" | "Testnet" | "Devnet" | string
-    url: string
+	chainId: string;
 }
 
-
 interface Account {
-    address: string
-    publicKey: string
+	address: string;
+	publicKey: string;
 }
 
 interface Transaction {
-    arguments: Array<string>
-    function: string
-    type: string
-    type_arguments: string[]
+	arguments: Array<string>;
+	function: string;
+	type: string;
+	type_arguments: string[];
 }
