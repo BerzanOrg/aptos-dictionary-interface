dd864674a5a7945cc0e16310a0087c9d95fe9511
Berzan <berzan@tutanota.com>
2023-09-26 14:22:21 +0000

    add: logic for adding new words

--- src/lib/components/LeftNavbar.svelte
+++ src/lib/components/LeftNavbar.svelte
@@ -1,14 +1,34 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
 	import { base } from '$app/paths';
 	import { contract } from '$lib/stores/contract';
 	import { wallet } from '$lib/stores/wallet';
+	import { Errors } from '$lib/utils/errors';
+
+	function addNewWordHandler() {
+		let newWord = prompt('Word to add:');
+
+		if (!newWord) return;
+		if ($contract.words.has(newWord)) throw Errors.WordAlreadyExists;
+
+		// replace spaces with hyphen
+		newWord = newWord
+			.toLowerCase()
+			.split(' ')
+			.map((s) => s.trim())
+			.filter((s) => s)
+			.join('-');
+
+		// redirect user to the new word's page
+		goto(`${base}/word/${newWord}`);
+	}
 </script>
 
 <nav
-	class="flex min-w-fit flex-col justify-between px-3 py-1 min-h-[calc(100vh-16rem)] bg-green-200 border border-green-300 rounded-lg">
+	class="flex min-w-fit flex-col justify-between px-3 pt-1 pb-5 min-h-[calc(100vh-16rem)] bg-green-200 border border-green-300 rounded-lg">
 	<div class="flex flex-col">
 		<h2 class="text-lg font-bold select-none">Words In The Dictionary</h2>
-		<div class="flex flex-col underline font-medium text-green-600">
+		<div class="flex flex-col gap-0.5 underline font-medium text-green-700">
 			{#if $contract?.words}
 				{#each $contract.words as [word]}
 					<a href="{base}/word/{word}">{word}</a>
@@ -16,10 +36,10 @@
 			{/if}
 		</div>
 	</div>
-	<div class="flex flex-col">
+	<div class="flex flex-col gap-1">
 		{#if $wallet.isConnected}
-			<a href="{base}/add-new-word" class="underline font-medium">Add New Word</a>
-			<a href="{base}/your-profile" class="underline font-medium">Your Profile</a>
+			<button on:click={addNewWordHandler} class="underline font-medium">Add New Word</button>
+			<a href="{base}/your-profile" class="underline font-medium text-center">Your Profile</a>
 		{/if}
 	</div>
 </nav>
