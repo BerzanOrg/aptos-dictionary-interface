ca9c5c53d7d69d33f8eac86638923c0169588ba0
Berzan <berzan@tutanota.com>
2023-09-26 14:24:17 +0000

    impl: support for adding definitions

--- src/routes/word/[word]/+page.svelte
+++ src/routes/word/[word]/+page.svelte
@@ -1,11 +1,26 @@
 <script lang="ts">
 	import { base } from '$app/paths';
 	import { contract } from '$lib/stores/contract.js';
-	contract;
+	import { wallet } from '$lib/stores/wallet.js';
+	import { Errors } from '$lib/utils/errors';
+	import { autoRowsHandler, disableNewlineHandler } from '$lib/utils/textarea.js';
 
 	export let data;
 
-	$: entries = $contract?.words.get(data.word);
+	/** Definitions for the word of the current page.*/
+	$: definitions = $contract?.words.get(data.word);
+
+	/** Binded value for definiton written in textarea. */
+	let definition: string = '';
+
+	/** Requests user to make a transaction to add a new definition for the word of the current page. */
+	async function addDefinition() {
+		if (definition === '' || !$wallet.address) throw Errors.WordDefinitionIsEmpty;
+
+		await contract.addDefinition(data.word, definition, $wallet.address);
+
+		definition = '';
+	}
 </script>
 
 <main
@@ -16,16 +31,16 @@
 		</h1>
 	</a>
 	<div class="flex flex-col gap-5">
-		{#if entries}
-			{#each entries as entry}
+		{#if definitions}
+			{#each definitions as definition}
 				<div
 					class="flex flex-col bg-green-300 border border-green-400 px-3 pt-1 pb-2 gap-2 rounded-lg">
-					<p class="font-semibold">{entry.definition}</p>
+					<p class="font-semibold">{definition.content}</p>
 					<div class="flex justify-end gap-3">
 						<div
 							class="flex flex-col items-end text-green-600 font-['Inconsolata',mono-space] text-sm font-medium">
-							<a href="{base}/author/{entry.author_addr}" class="underline">
-								{entry.author_addr}
+							<a href="{base}/author/{definition.author_addr}" class="underline">
+								{definition.author_addr}
 							</a>
 							<p>
 								<!--
@@ -37,8 +52,9 @@
 							</p>
 						</div>
 						<img
-							src={$contract?.authors.get(entry.author_addr)?.profile_picture}
-							alt="profile photo of author {entry.author_addr}"
+							src={$contract?.authorProfiles.get(definition.author_addr)?.picture ||
+								`${base}/default_profile_picture.png`}
+							alt="profile photo of author {definition.author_addr}"
 							class="h-11 w-11 rounded-full border border-green-500" />
 					</div>
 				</div>
@@ -46,5 +62,27 @@
 		{:else}
 			<p>There is no entry</p>
 		{/if}
+
+		<div
+			class="flex bg-green-100 border border-green-400 px-3 pt-1.5 pb-3 gap-3 rounded-lg items-end {!$wallet.isConnected &&
+				'cursor-not-allowed'}">
+			<textarea
+				disabled={!$wallet.isConnected}
+				rows="4"
+				maxlength="1024"
+				placeholder={$wallet.isConnected
+					? `Write your own definition for ${data.word}...`
+					: `You must connect your wallet before adding your own definition for ${data.word}...`}
+				bind:value={definition}
+				on:keydown={disableNewlineHandler}
+				on:input={autoRowsHandler}
+				class="w-full bg-green-100 text-green-700 outline-none resize-none placeholder:text-green-700/50 disabled:cursor-not-allowed" />
+			<button
+				disabled={!$wallet.isConnected}
+				on:click={addDefinition}
+				class="font-bold bg-green-300 px-3 border text-green-700 border-green-500 py-1 rounded-full hover:bg-green-400/80 disabled:hover:bg-green-300 duration-15 disabled:cursor-not-allowed">
+				Add
+			</button>
+		</div>
 	</div>
 </main>
