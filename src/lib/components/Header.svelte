f9ca5bec7ad07ee2601b100067407f138fbf462f
Berzan <berzan@tutanota.com>
2023-09-27 19:19:21 +0000

    update: styling

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -4,10 +4,10 @@
 </script>
 
 <header
-	class="sticky top-0 z-30 flex justify-between items-center h-16 bg-green-300/50 border-b border-green-300 backdrop-blur-sm px-6 select-none">
+	class="sticky top-0 z-30 flex justify-between items-center h-16 bg-green-300/50 border-b border-green-400 backdrop-blur-sm px-6 select-none">
 	<a href="{base}/" class="text-2xl font-bold">Aptos Dictionary</a>
 	<button
-		class="font-bold bg-green-400/60 hover:bg-green-500/60 border border-green-500 px-4 py-1 rounded-full duration-150"
+		class="font-bold bg-green-400 hover:bg-green-500 border border-green-600 px-5 h-10 text-lg rounded-full duration-150"
 		on:click={$wallet.isConnected ? wallet.disconnect : wallet.connect}>
 		{#if $wallet.isConnected}
 			Disconnect
