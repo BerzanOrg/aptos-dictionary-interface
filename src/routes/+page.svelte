92d8e63cec0f4996d8607225e1f1cdbbdb6d6c31
Berzan <berzan@tutanota.com>
2023-09-27 19:22:27 +0000

    use: components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { base } from '$app/paths';
+	import Main from '$lib/components/Main.svelte';
 	import { contract } from '$lib/stores/contract';
 
 	$: getRandomWordUrl = () => {
@@ -11,28 +12,29 @@
 	};
 </script>
 
-<main
-	class="flex flex-col px-3 py-2 bg-green-200 border border-green-300 rounded-lg w-full gap-6 justify-center">
-	<div class="flex flex-col gap-2">
-		<h1 class="text-4xl font-bold text-green-700 text-center">Welcome to Aptos Dictionary</h1>
-		<p class="font-semibold text-center text-green-600">
-			You are in the colloborative dictionary of Aptos. Anyone can be an author of this great
-			dictionary.
-		</p>
-	</div>
+<Main>
+	<div class="h-full flex flex-col px-3 py-2 gap-6 justify-center">
+		<div class="flex flex-col gap-2">
+			<h1 class="text-4xl font-bold text-green-700 text-center">Welcome to Aptos Dictionary</h1>
+			<p class="font-semibold text-center text-green-600">
+				You are in the colloborative dictionary of Aptos. Anyone can be an author of this great
+				dictionary.
+			</p>
+		</div>
 
-	<div class="flex max-w-md self-center gap-8">
-		<a
-			href={getRandomWordUrl()}
-			class="font-bold bg-green-400/60 hover:bg-green-500/60 border border-green-500 px-4 py-1 rounded-full duration-150">
-			Visit a Random Page
-		</a>
+		<div class="flex max-w-md self-center gap-8">
+			<a
+				href={getRandomWordUrl()}
+				class="font-bold bg-green-400/60 hover:bg-green-500/60 border border-green-500 px-4 py-1 rounded-full duration-150">
+				Visit a Random Page
+			</a>
 
-		<a
-			href="https://github.com/BerzanXYZ/aptos-dictionary-interface"
-			class="font-bold bg-green-400/60 hover:bg-green-500/60 border border-green-500 px-4 py-1 rounded-full duration-150"
-			target="_blank">
-			Visit the Repository
-		</a>
+			<a
+				href="https://github.com/BerzanXYZ/aptos-dictionary-interface"
+				class="font-bold bg-green-400/60 hover:bg-green-500/60 border border-green-500 px-4 py-1 rounded-full duration-150"
+				target="_blank">
+				Visit the Repository
+			</a>
+		</div>
 	</div>
-</main>
+</Main>
