7f57f8b3b21f9320badda2365502349fe8d33661
Berzan <berzan@tutanota.com>
2023-09-27 19:19:53 +0000

    update: styling

--- src/lib/components/LeftNavbar.svelte
+++ src/lib/components/LeftNavbar.svelte
@@ -25,21 +25,24 @@
 </script>
 
 <nav
-	class="flex min-w-fit flex-col justify-between px-3 pt-1 pb-5 min-h-[calc(100vh-16rem)] bg-green-200 border border-green-300 rounded-lg">
-	<div class="flex flex-col">
-		<h2 class="text-lg font-bold select-none">Words In The Dictionary</h2>
-		<div class="flex flex-col gap-0.5 underline font-medium text-green-700">
-			{#if $contract?.definitions}
-				{#each $contract.definitions as [word]}
-					<a href="{base}/word/{word}">{word}</a>
-				{/each}
-			{/if}
+	class="flex min-w-fit flex-col justify-between px-3 pt-1.5 pb-5 min-h-[calc(100vh-16rem)] bg-green-300/50 backdrop-blur-sm border border-green-400 rounded-xl">
+	<div class="flex flex-col gap-1 items-start">
+		<h2 class="text-xl font-bold select-none">Words In The Dictionary</h2>
+		<div class="flex flex-col gap-0.5 underline font-semibold text-green-800">
+			{#each $contract.definitions.keys() as word}
+				<a href="{base}/word/{word}" class="hover:text-green-600"> {word} </a>
+			{/each}
 		</div>
 	</div>
-	<div class="flex flex-col gap-1">
-		{#if $wallet.isConnected}
-			<button on:click={addNewWordHandler} class="underline font-medium">Add New Word</button>
-			<a href="{base}/your-profile" class="underline font-medium text-center">Your Profile</a>
-		{/if}
+	<div class="flex flex-col gap-1 font-bold items-center text-green-900">
+		<button
+			on:click={addNewWordHandler}
+			disabled={!$wallet.isConnected}
+			class="hover:text-green-700 hover:underline disabled:cursor-not-allowed">
+			Add New Word</button>
+		<a
+			href={$wallet.address ? `${base}/author/${$wallet.address}` : null}
+			class="hover:text-green-700 hover:underline {!$wallet.address && 'cursor-not-allowed'}">
+			Your Profile</a>
 	</div>
 </nav>
